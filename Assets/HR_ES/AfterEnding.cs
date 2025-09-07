0b13d6959be153115c68df3bd5cf0b4007b88f71
hlj1085 <113406145+hlj1085@users.noreply.github.com>
2025-09-07 14:59:04 +0900

    로비&AfterGame

--- Assets/HR_ES/AfterEnding.cs
+++ Assets/HR_ES/AfterEnding.cs
@@ -1,18 +1,23 @@
+using Photon.Pun;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class AfterEnding : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    // 다음 게임을 위해 방으로 돌아가는 함수
+    public void GoToRoomScene()
     {
-        
+        Debug.Log("방으로 돌아갑니다.");
+        SceneManager.LoadScene("RoomScene");
     }
 
-    // Update is called once per frame
-    void Update()
+    // 방에서 나가고 메인 화면으로 돌아가는 함수
+    public void ExitToMain()
     {
-        
+        Debug.Log("게임을 나가고 메인 화면으로 돌아갑니다.");
+        PhotonNetwork.LeaveRoom(); // 현재 방에서 나가기
+        SceneManager.LoadScene("Server_1"); // 메인 화면(Server_1) 씬 로드
     }
 }
